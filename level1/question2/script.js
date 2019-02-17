ffb67e653699c4c9aa9d946b11d87802e4c2e331
Alicia Skutle <alicia.skutle@gmail.com>
2019-02-17 22:42:37 +0100

    v2

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,30 @@
 //Console.log out elements in JSON file
+
+var videoData = {
+  video: [
+    {
+      id: 12312412312,
+      name: "Ecuaciones Diferenciales",
+      url: "/video/math/edo/12312412312",
+      author: {
+        data: [
+          {
+            name_author: "Alejandro Morales",
+            url: "/author/alejandro-morales",
+            type: "master"
+          }
+        ]
+      }
+    }
+  ]
+};
+
+for (i = 0; i < videoData.video.length; i++) {
+  output = videoData.video[i];
+  {
+    for (i = 0; i < videoData.video.data.length; i++) {
+      output = videoData.video.data[i];
+    }
+    console.log(videoData.video[i]);
+  }
+}
